b09909b1420f9998e35549e8fa0b452952f74459
predragBgd <pecabegd@gmail.com>
2023-01-28 11:06:25 +0100

    vezba

--- vezbanje123.js
+++ vezbanje123.js
@@ -78,8 +78,62 @@ trougao2(3);
 function trougao3(d) {
   let i = 3;
   console.log(a.repeat(d));
-  while (i--) {
-    console.log((b + a).repeat(i));
+  while (i) {
+    console.log(a + b.repeat(i - 1) + a);
+    i--;
   }
+  console.log(a);
 }
 trougao3(5);
+
+console.log("Kocka");
+let prvi = " ";
+let drugi = "/\\";
+let treci = "_\\";
+let cetvrti = "\\/";
+let peti = "_/";
+
+function kocka(n) {
+  let i = 0;
+  let m = n;
+  while (i < n) {
+    console.log(prvi.repeat(m - 1) + drugi.repeat(i + 1) + treci.repeat(n));
+    i++;
+    m--;
+  }
+  while (i) {
+    console.log(prvi.repeat(m) + cetvrti.repeat(i) + peti.repeat(n));
+    i--;
+    m++;
+  }
+}
+kocka(3);
+
+// Nalazenje srednje vrednosti i medijalnog broja u nizu
+
+let arr = [0, 3, 8, 3, 1, 9, 11];
+let sumNo = 0;
+for (let i = 0; i < arr.length; i++) {
+  sumNo += arr[i];
+}
+console.log(sumNo);
+
+let srednjaVrednost = sumNo / arr.length;
+console.log(srednjaVrednost);
+
+let arrSlozen = arr.sort((a, b) => {
+  return a - b;
+});
+console.log(arrSlozen);
+
+if (arrSlozen.length % 2 != 0) {
+  let indexBr = Math.ceil(arrSlozen.length / 2);
+  console.log("yyyy" + arrSlozen[indexBr - 1]);
+}
+if (arrSlozen.length % 2 == 0) {
+  let indexBr = arrSlozen.length / 2;
+  console.log("uuuu" + (arrSlozen[indexBr - 1] + arrSlozen[indexBr]) / 2);
+}
+//console.log(arrSlozen[arrSlozen.length / 2]);
+
+// let medijalnaVrednost =
